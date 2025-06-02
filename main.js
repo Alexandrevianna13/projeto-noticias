ad7ca8fe79d7c5d4ee7ce6453dd910d540360f33
5518alezin <5518alezin@gmail.com>
2025-06-02 02:18:37 +0000

    javascript para abir e fechar o menu completo

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function main() {
-  console.log("Hello, World!");
+const BotaoAbrir = document.querySelector(".header-mobile > button")
+const BotaoFechar = document.querySelector(".header-mobile nav button")
+
+const body = querySelector("body")
+const nav = querySelector(".header-mobile nav")
+
+BotaoAbrir.addEventListener("click", )
+
+function abrirMenu() {
+  body.classList.add("escurecer")
+  nav.classList.add("abrir")
+
 }
 
-main();
+function fecharMenu() {
+  body.classList.remove("escurecer")
+  nav.classList.remove("abrir")
+}
