e330186a993ba182463db267e65a86eabee00851
5518alezin <5518alezin@gmail.com>
2025-06-10 10:46:21 +0000

    correçao: algumas digitaçoes erradas no js e css

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const BotaoAbrir = document.querySelector(".header > button")
-const BotaoFechar = document.querySelector(".header nav button")
+const botaoAbrir = document.querySelector("header > button");
+const botaoFechar = document.querySelector("header nav button");
 
-const body = querySelector("body")
-const nav = querySelector(".header .mobile")
+const body = document.querySelector("body");
+const nav = document.querySelector("header .mobile");
 
-BotaoAbrir.addEventListener("click", )
+botaoAbrir.addEventListener('click', abrirMenu);
+botaoFechar.addEventListener('click', fecharMenu);
 
 function abrirMenu() {
-  body.classList.add("escurecer")
-  nav.classList.add("abrir")
-
+  body.classList.add("escurecer");
+  nav.classList.add("abrir");
 }
 
 function fecharMenu() {
-  body.classList.remove("escurecer")
-  nav.classList.remove("abrir")
+  body.classList.remove("escurecer");
+  nav.classList.remove("abrir");
 }
